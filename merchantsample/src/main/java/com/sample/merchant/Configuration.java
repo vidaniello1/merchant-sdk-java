7078fe8e8300e42df462778f0f1f13b6c28b975b
tkanta <tarininegi@rediffmail.com>
2013-07-12 15:54:20 -0700

    appId removed

--- merchantsample/src/main/java/com/sample/merchant/Configuration.java
+++ merchantsample/src/main/java/com/sample/merchant/Configuration.java
@@ -20,7 +20,6 @@ public class Configuration {
 		configMap.put("acct1.UserName", "jb-us-seller_api1.paypal.com");
 		configMap.put("acct1.Password", "WX4WTU3S8MY44S7F");
 		configMap.put("acct1.Signature", "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy");
-		configMap.put("acct1.AppId", "APP-80W284485P519543T");
 		// Subject is optional, only required in case of third party permission
 		//configMap.put("acct1.Subject", "");
 		
@@ -40,7 +39,6 @@ public class Configuration {
 		configMap.put("acct2.Password", "D6JNKKULHN3G5B8A");
 		configMap.put("acct2.CertKey", "password");
 		configMap.put("acct2.CertPath", "resource/sdk-cert.p12");
-		configMap.put("acct2.AppId", "APP-80W284485P519543T");
 		// Subject is optional, only required in case of third party permission
 		//configMap.put("acct2.Subject", "");
 		
