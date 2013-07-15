d0578e7739de90f19fe6c3cf0a993bd8c110acae
tkanta <tarininegi@rediffmail.com>
2013-07-15 11:42:06 -0700

    dynamic config update

--- merchantsample/src/main/java/com/sample/merchant/Configuration.java
+++ merchantsample/src/main/java/com/sample/merchant/Configuration.java
@@ -46,9 +46,9 @@ public class Configuration {
 	}
 	
 	public static final Map<String,String> getMode(){
-		Map<String,String> configMap = new HashMap<String,String>();
+		Map<String,String> mode = new HashMap<String,String>();
 		// Endpoints are varied depending on whether sandbox OR live is chosen for mode
-		configMap.put("mode", "sandbox");
-		return configMap;
+		mode.put("mode", "sandbox");
+		return mode;
 	}
 }
